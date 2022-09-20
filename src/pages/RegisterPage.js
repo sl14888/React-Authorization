4c4084863dd1992f7346f425a61bc8359e1fc7a7
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-09-20 22:48:28 +0300

    сверстал страницу регистрации

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,13 +1,91 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Divider, Form, Input } from 'antd';
+
 const RegisterPage = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
-      <p>
-        Already have an account? <Link to="/login">Sign in</Link>
-      </p>
+    <div className="register">
+      <div className="register-wrapper">
+        <Divider className="register-divider">Register</Divider>
+
+        <Form
+          name="normal_register"
+          className="register-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="confirm"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error('The two passwords that you entered do not match!')
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              placeholder="Confirm Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="register-form-button">
+              register
+            </Button>
+            Or <Link to="/login">Sign in!</Link>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
